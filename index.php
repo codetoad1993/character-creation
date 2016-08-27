a8ec7b810277b23b1a557bc5e5350ab445989241
codetoad1993 <codytetreault@gmail.com>
2016-08-27 02:17:07 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Myth Create
+* Plugin URI: http://wordpress.com/
+* Description: Create and manage characters.
+* Version: 1.0
+* Author: Cody Tetreault
+* Author URI: http://www.Mythlarp.com
+* License: #ML
+
+{myth_create} is not intended to be to be used by any other party than the original creator. This plugin is designed only for our website, http://mythlarp.com.
+*/
+
+
+?>
